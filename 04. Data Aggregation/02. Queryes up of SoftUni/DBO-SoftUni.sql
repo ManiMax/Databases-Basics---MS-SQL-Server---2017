a7513e7f39742d677aac7983a4c66c05c47c2671
Stoyan Zlatev <spzvtbg@mail.de>
2017-10-05 22:19:07 +0200

    MY SOLUTIONS 100 / 100

--- 04. Data Aggregation/02. Queryes up of SoftUni/DBO-SoftUni.sql
+++ 04. Data Aggregation/02. Queryes up of SoftUni/DBO-SoftUni.sql
@@ -66,11 +66,16 @@ FROM
 WHERE Rank LIKE 3;
 
 --- 19. Salary Challenge ---
-SELECT TOP 10 FirstName, LastName, DepartmentID FROM Employees AS emp1
-WHERE Salary >
-(SELECT AVG(Salary) FROM Employees AS emp2
-WHERE emp1.DepartmentID = emp2.DepartmentID
-GROUP BY DepartmentID)
+SELECT TOP(10) 
+	FirstName, 
+	LastName, 
+	DepartmentID 
+FROM Employees AS emp1
+WHERE Salary >  (
+					SELECT AVG(Salary) FROM Employees AS emp2
+					WHERE emp1.DepartmentID = emp2.DepartmentID
+					GROUP BY DepartmentID
+				)
 ORDER BY DepartmentID
 
 
