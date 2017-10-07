76e26b39e1f714e4a0c1b2f80af823995c77d143
Stoyan Zlatev <spzvtbg@mail.de>
2017-10-07 10:03:58 +0200

    MY SOLUTIONS 100 / 100

---
+++ 05. Table Relations/05. Online Store Database/Online Store Database.sql
@@ -0,0 +1,82 @@
+CREATE DATABASE [Online Store];
+GO
+
+USE [Online Store];
+GO
+
+CREATE TABLE Cities
+(
+	CityID INT IDENTITY(1, 1),
+	Name VARCHAR(50) NOT NULL,
+
+	CONSTRAINT PK_Cities 
+	PRIMARY KEY (CityID)
+);
+
+CREATE TABLE Costumers
+(
+	CostumerID INT IDENTITY(1, 1),
+	Name VARCHAR(50) NOT NULL,
+	Birthday DATE,
+	CityID INT,
+
+	CONSTRAINT PK_Costumers 
+	PRIMARY KEY (CostumerID),
+
+	CONSTRAINT FK_Costumers 
+	FOREIGN KEY (CityID) 
+	REFERENCES Cities(CityID)
+);
+
+CREATE TABLE Orders
+(
+	OrderID INT IDENTITY(1, 1),
+	CostumerID INT NOT NULL,
+
+	CONSTRAINT PK_Orders 
+	PRIMARY KEY (OrderID),
+
+	CONSTRAINT FK_Orders
+	FOREIGN KEY (CostumerID)
+	REFERENCES Costumers(CostumerID)
+);
+
+CREATE TABLE ItemTypes
+(
+	ItemTypeID INT IDENTITY(1, 1),
+	Name VARCHAR(50) NOT NULL,
+
+	CONSTRAINT PK_ItemTypes 
+	PRIMARY KEY (ItemTypeID)
+);
+
+CREATE TABLE Items
+(
+	ItemID INT IDENTITY(1, 1),
+	Name VARCHAR(50) NOT NULL,
+	ItemTypeID INT NOT NULL,
+
+	CONSTRAINT PK_Items 
+	PRIMARY KEY (ItemID),
+
+	CONSTRAINT FK_Items 
+	FOREIGN KEY (ItemTypeID) 
+	REFERENCES ItemTypes(ItemTypeID)
+);
+
+CREATE TABLE OredersItems
+(
+	OrderID INT NOT NULL,
+	ItemID INT NOT NULL,
+
+	CONSTRAINT PK_OredersItems 
+	PRIMARY KEY (OrderID, ItemID),
+
+    CONSTRAINT FK_OrdersItems_Orders
+	FOREIGN KEY (OrderID)
+	REFERENCES Orders(OrderID),
+
+	CONSTRAINT FK_OrdersItems_Items
+	FOREIGN KEY (ItemID)
+	REFERENCES Items(ItemID)
+);
